6465ab6ec23e13f684bb416ec332eaac1633c066
Agata Musiuk <agata.musiuk@gmail.com>
2019-06-29 09:39:50 +0200

    Update modal, fix errors

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-//sidebar
-
-let navWrapper = document.querySelector('.nav-wrapper'),
-	navToogler = document.querySelector('.nav-toogler')
-
-navToogler.addEventListener('click', function (event) {
-	navWrapper.classList.toggle('active')
-})
-
-//postback --- range slider
-
-var slider = document.getElementById("rangeSlider");
-var output = document.getElementById("rangeValue");
-output.innerHTML = slider.value + ' hours';
-
-slider.oninput = function () {
-	output.innerHTML = this.value;
-}
-
 //closing modals
 
 function closeModal() {
@@ -54,8 +35,103 @@ function openModal(modal) {
 
 //opening addUrlModal
 
-document.querySelector('#addUrlModal') {
-	btn.addEventListener('click', function () {
-		openModal('#addUrlModal')
+var urlTop = document.querySelector('#addUrlModalTop');
+var urlBottom = document.querySelector('#addUrlModalBottom');
+
+if(urlTop) {
+	urlTop.addEventListener('click', function () {
+		openModal('#myModal')
 	})
 }
+
+if(urlBottom) {
+	urlBottom.addEventListener('click', function () {
+		openModal('#myModal')
+	})
+}
+
+// slider ---- moving value bubble
+
+function modifyOffset() {
+    var el, newPoint, newPlace, offset, siblings, k;
+    width    = this.offsetWidth;
+    newPoint = (this.value - this.getAttribute("min")) / (this.getAttribute("max") - this.getAttribute("min"));
+    offset   = -1;
+    if (newPoint < 0) { newPlace = 0;  }
+    else if (newPoint > 1) { newPlace = width; }
+    else { newPlace = width * newPoint + offset; offset -= newPoint;}
+    siblings = this.parentNode.childNodes;
+    for (var i = 0; i < siblings.length; i++) {
+        sibling = siblings[i];
+        if (sibling.id == this.id) { k = true; }
+        if ((k == true) && (sibling.nodeName == "OUTPUT")) {
+            outputTag = sibling;
+        }
+    }
+    outputTag.style.left       = newPlace + "px";
+    outputTag.style.marginLeft = offset + "%";
+    outputTag.innerHTML        = this.value;
+}
+
+function modifyInputs() {
+    
+    var inputs = document.getElementById("rangeSlider");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].getAttribute("type") == "range") {
+			inputs[i].onchange = modifyOffset;
+			
+            if ("fireEvent" in inputs[i]) {
+                inputs[i].fireEvent("onchange");
+            } else {
+                var evt = document.createEvent("HTMLEvents");
+                evt.initEvent("change", false, true);
+                inputs[i].dispatchEvent(evt);
+            }
+        }
+    }
+}
+
+modifyInputs();
+
+/*
+//postback --- range slider --- old version, not moving
+
+var slider = document.getElementById("rangeSlider");
+var output = document.getElementById("rangeValue");
+output.innerHTML = slider.value;
+
+if(slider) {
+	slider.addEventListener('change', function() {
+		output.innerHTML = slider.value;
+	})
+}
+*/
+
+//general --- chart configuration
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'bar', // chart choice
+    data: {
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"], //x axis labels
+        datasets: [{ //chart series
+            label: "Signups", //series label
+            backgroundColor: '#8DBEC8', //series color
+            borderColor: '#8DBEC8',
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ], //series dataset
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+            hidden: true, //series not visible on chart
+        }]
+    },
+});
