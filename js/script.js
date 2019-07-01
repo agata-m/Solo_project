7b6a0ae12c4b00d11b480559732cf21f7b94415e
Agata Musiuk <agata.musiuk@gmail.com>
2019-07-01 11:58:36 +0200

    Add flags, update RWD

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// jQuery flags selector
+
+function onChangeCallback(ctr) {
+    console.log("The country was changed: " + ctr);
+}
+
+$(function() {
+    new NiceCountryInput($("#flags")).init();
+});
+
+
 //closing modals
 
 function closeModal() {
@@ -23,6 +34,7 @@ document.addEventListener('keyup', function (e) {
 	}
 })
 
+
 //opening modals
 
 function openModal(modal) {
@@ -32,6 +44,7 @@ function openModal(modal) {
 	document.querySelector('#overlay').classList.add('show')
 	document.querySelector(modal).classList.add('show')
 }
+
 
 //opening addUrlModal
 
@@ -50,62 +63,19 @@ if(urlBottom) {
 	})
 }
 
-// slider ---- moving value bubble
 
-function modifyOffset() {
-    var el, newPoint, newPlace, offset, siblings, k;
-    width    = this.offsetWidth;
-    newPoint = (this.value - this.getAttribute("min")) / (this.getAttribute("max") - this.getAttribute("min"));
-    offset   = -1;
-    if (newPoint < 0) { newPlace = 0;  }
-    else if (newPoint > 1) { newPlace = width; }
-    else { newPlace = width * newPoint + offset; offset -= newPoint;}
-    siblings = this.parentNode.childNodes;
-    for (var i = 0; i < siblings.length; i++) {
-        sibling = siblings[i];
-        if (sibling.id == this.id) { k = true; }
-        if ((k == true) && (sibling.nodeName == "OUTPUT")) {
-            outputTag = sibling;
-        }
-    }
-    outputTag.style.left       = newPlace + "px";
-    outputTag.style.marginLeft = offset + "%";
-    outputTag.innerHTML        = this.value;
-}
-
-function modifyInputs() {
-    
-    var inputs = document.getElementById("rangeSlider");
-    for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].getAttribute("type") == "range") {
-			inputs[i].onchange = modifyOffset;
-			
-            if ("fireEvent" in inputs[i]) {
-                inputs[i].fireEvent("onchange");
-            } else {
-                var evt = document.createEvent("HTMLEvents");
-                evt.initEvent("change", false, true);
-                inputs[i].dispatchEvent(evt);
-            }
-        }
-    }
-}
-
-modifyInputs();
-
-/*
 //postback --- range slider --- old version, not moving
 
 var slider = document.getElementById("rangeSlider");
 var output = document.getElementById("rangeValue");
-output.innerHTML = slider.value;
+output.innerHTML = slider.value + ' hours';
 
 if(slider) {
 	slider.addEventListener('change', function() {
-		output.innerHTML = slider.value;
+		output.innerHTML = slider.value + ' hours';
 	})
 }
-*/
+
 
 //general --- chart configuration
 
